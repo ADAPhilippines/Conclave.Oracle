79ff79be8a83949dcf507a8ebb864c1aa78b8ef4
rjlacanlaled <rjlacanlaled@gmail.com>
2022-11-08 17:26:05 +0800

    define interface for staking

---
+++ src/Conclave.Oracle.Contract/contracts/interfaces/IStakeable.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+interface IStakeable {
+    function stake(uint256 amount) external;
+
+    function unstake(uint256 amount) external;
+
+    function getStake() external view returns (uint256);
+
+    function getStake(address account) external view returns (uint256);
+}
