0feb5c0f3c5a89cf54e09127f47045952e5bc1f3
rjlacanlaled <rjlacanlaled@gmail.com>
2022-11-11 20:46:44 +0800

    implement oracle consumer interface

--- src/Conclave.Oracle.Contract/contracts/abstracts/ConclaveOracleConsumerBase.sol
+++ src/Conclave.Oracle.Contract/contracts/abstracts/ConclaveOracleConsumerBase.sol
@@ -1,17 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-interface IConclaveOracleConsumer {
+import "../interfaces/IConclaveOracleConsumer.sol";
+
+abstract contract ConclaveOracleConsumerBase is IConclaveOracleConsumer {
     function requestRandomNumber(
         uint256 numCount,
         uint256 fee,
         uint256 tokenFee
-    ) external;
+    ) external {}
 
     function getResponseCount(uint256 jobId)
         external
         view
-        returns (uint256 currentResponses, uint256 totalResponses);
+        returns (uint256 currentResponses, uint256 totalResponses)
+    {}
 
-    function aggregateResponses(uint256 jobId) external;
+    function aggregateResponses(uint256 jobId) external {}
+
+    function _fulfillRandomNumbers(
+        uint256 requestId,
+        uint256[] memory randomNumbers
+    ) internal virtual;
 }
