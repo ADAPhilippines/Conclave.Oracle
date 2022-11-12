424bec8967985721706a8bf6dcbc071cc3042b58
rjlacanlaled <rjlacanlaled@gmail.com>
2022-11-12 15:11:12 +0800

    add aggregator interface

--- src/Conclave.Oracle.Contract/contracts/interfaces/IConclaveOracle.sol
+++ src/Conclave.Oracle.Contract/contracts/interfaces/IConclaveOracle.sol
@@ -3,10 +3,12 @@ pragma solidity ^0.8.17;
 
 interface IConclaveOracle {
     function requestRandomNumbers(
-        uint32 numCount,
+        uint24 numCount,
         uint256 fee,
         uint256 feePerNum,
         uint256 tokenFee,
         uint256 tokenFeePerNum
     ) external payable returns (uint256 jobId);
+
+    function aggregateResult(uint256 jobId) external returns (uint256);
 }
